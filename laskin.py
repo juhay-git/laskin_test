1e68992a62bf6c2c50a33a45b6bf6b018895c756
juhay-git <juha.yli-hemminki@seamk.fi>
2024-09-25 14:52:45 +0300

    laskimen ensimmäinen versio

---
+++ laskin.py
@@ -0,0 +1,9 @@
+from math_function import add
+
+def main():
+    luku1 = int(input("Anna luku 1 > "))
+    luku2 = int(input("Anna luku 2 > "))
+    print("Tulos > ", add(luku1, luku2))
+
+if __name__ == "__main__":
+    main()
---
+++ math_function.py
@@ -0,0 +1,4 @@
+# math_function.py
+
+def add(x, y):
+    return x+y
